7da826c28cc4b5a0d6dca957272373a53d5f77a0
Tonda Horkel <59200680+TonikHorkel@users.noreply.github.com>
2022-07-11 12:33:44 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,11 +1,13 @@
 import random
 
 # ==== Generování náhodné mapy. ====
-# Podle mě nejjednodušší způsob jak vygenerovat 8*8 matci s 32 náhodně rozmístěnými nepřáteli je...
-# předdefinovat matici s 32 prázdnými místy ('█') a 32 nepřáteli ('O') a pomocí funkce random.shuffle() ji náhodně zamíchat.
+# Podle mě nejjednodušší způsob jak vygenerovat 8*8 matci s 32 náhodně rozmístěnými zlouny je...
+# předdefinovat matici s 32 prázdnými místy ('█') a 32 zlouny ('O') a pomocí funkce random.shuffle() ji náhodně zamíchat.
 # Tento přístup má ovšem jednu nevýhodu: random.shuffle() neumí míchat vícedimenzionální matice.
 # Proto je nutné přeměnit 2D matici s rozměry 8*8 na 1D matici s rozměry 1*64, a jen předstírat že je 2D.
-# Indexování pak funguje takto: mat1D[x + y * 8] = mat2D[x][y] což znamená: mat1D[0...7] = mat2D[0][0...7], mat1D[8...15] = mat2D[1][0...7] atd.
+# Indexování pak funguje takto: mat1D[x + y * 8] = mat2D[y][x] což znamená: mat1D[0...7] = mat2D[0][0...7], mat1D[8...15] = mat2D[1][0...7] atd.
+# Také není nutné, aby mapa (vygenerovaná matice) ukládala u zlounů jejich sílu, počet živoutů a odolnost, protože...
+# hrdina vždy bojuje maximálně s jedním za kolo a tyto vlastnosti mohou být vygenerovány až před bojem.
 mapa = list("████████████████████████████████OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO") # Definice matice s rozměry 1*64.
 random.shuffle(mapa) # Zamíchání matice.
 # mapa.insert(0, '█')
